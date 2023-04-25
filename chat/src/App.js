963bc488e6b3973e99d9933f7067a2e27f807b09
Dheeraj-2205 <djsays124@gmail.com>
2023-04-26 01:09:27 +0530

    using chakra ui

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,11 +1,28 @@
+import {Box,ButtonGroup,Container,VStack,Button, Input} from "@chakra-ui/react"
 
-import './App.css';
 
 function App() {
+  console.log("object");
   return (
-    <div className="App">
-      
-    </div>
+    <Box bg ={"red.50"}>
+      <Container h ={"100vh"} bg = {"white"}>
+          <VStack h= {"full"} bg = {"telegram.100"}>
+            <Button w = {"full"} colorScheme = {"blue"}>
+              Logout
+            </Button>
+            <VStack h= "full" w = "full" bg = "purple.100">
+
+            </VStack>
+
+            <form>
+              <Input/>
+              <Button colorScheme = {"purple.100"} type = "submit">
+
+              </Button>
+            </form>
+          </VStack>
+      </Container>
+    </Box>
   );
 }
 
